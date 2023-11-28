6df84de58facdd7b91cbab80aa841af9783e71e7
davidchamorro2023 <130484306+davidchamorro2023@users.noreply.github.com>
2023-11-28 16:12:34 -0300

    Incorporación del server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,32 @@
-import fs from "node:fs"
+import net from "node:net";
+import dotenv from "dotenv";
+dotenv.config();
+
+let port = process.env.PORT ?? 2323;
+
+//if(!port) {
+//    port =2323;
+//}
+
+const serverTCP = net.createServer();
+
+serverTCP.on("connection", (socket) => {
+    socket.on("data", () => {
+
+
+    });
+
+    socket.on("close", () => {
+        console.log("Client disconnected");
+    });
+
+    socket.on("error", () => {
+        console.log("Client error");
+    });
+
+    console.log("Client connected", new Date().toLocaleString());
+});
+
+serverTCP.listen(port, () => {
+    console.log(`server is up on port ${port} - ${ new Date().toLocaleString()}`);
+});
